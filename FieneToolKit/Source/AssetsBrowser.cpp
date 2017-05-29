7f076aa58662deea0de13668ff91568c40507467
stelro <stelmach.ro@gmail.com>
2017-05-29 17:40:27 +0300

    editor changes

---
+++ FieneToolKit/Source/AssetsBrowser.cpp
@@ -0,0 +1,19 @@
+//
+// Created by stel on 28/5/2017.
+//
+
+#include "AssetsBrowser.hpp"
+
+
+AssetsBrowser::AssetsBrowser()
+{
+
+}
+
+AssetsBrowser::~AssetsBrowser()
+{
+
+}
+
+
+
---
+++ FieneToolKit/Source/AssetsBrowser.hpp
@@ -0,0 +1,21 @@
+//
+// Created by stel on 28/5/2017.
+//
+
+#ifndef PROJECT_ASSETSBROWSER_HPP
+#define PROJECT_ASSETSBROWSER_HPP
+
+
+
+class AssetsBrowser {
+public:
+    AssetsBrowser();
+    ~AssetsBrowser();
+
+private:
+
+};
+
+
+
+#endif //PROJECT_ASSETSBROWSER_HPP
